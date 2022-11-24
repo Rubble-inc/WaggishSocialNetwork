a0cf9efc72aa1a079ee63d0068d9733805e9304c
BenitoSwaggolini <profie_player@mail.ru>
2022-11-24 18:46:01 +0500

    + models

--- src/user/models.py
+++ src/user/models.py
@@ -1,1 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
+
+
+class Friendship(models.Model):
+    user_id = models.IntegerField()
+    accepted = models.BooleanField(default=False)
+
+
+class Person(AbstractUser):
+    photo = models.ImageField(blank=True, upload_to='media/photos/')
+    friends = models.ForeignKey(Friendship, related_name='friends', on_delete=models.PROTECT, blank=True)
+
+
+class UserProfile(models.Model):
+    info = models.TextField(blank=True)
+    user = models.OneToOneField(Person, on_delete=models.CASCADE)
+
+
+
+
+
+
+
+
+
+
+
